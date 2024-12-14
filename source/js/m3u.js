85ce7170b431fbbfa375170041b401b03a15dee9
jinkai.chen <chenjkai@163.com>
2024-12-14 20:35:57 +0800

    增加cctv2

--- source/js/m3u.js
+++ source/js/m3u.js
@@ -1,3 +1,5 @@
 #EXTM3U
 #EXTINF:-1 tvg-id="cctv1" tvg-logo="" group-title="",cctv1
 http://118.248.167.203:8088/tsfile/live/0001_1.m3u8
+#EXTINF:-1 tvg-id="cctv2" tvg-logo="" group-title="",cctv2
+http://171.213.203.181:8088/udp/239.93.1.23:6000
