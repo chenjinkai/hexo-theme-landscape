d8eb7384d9eaa1e557fa027b6bdee41de5211900
jinkai.chen <chenjkai@163.com>
2025-08-09 15:25:47 +0800

    Delete m3u.js

--- source/js/m3u.js
+++
@@ -1,9 +0,0 @@
-#EXTM3U
-#EXTINF:-1 tvg-id="cctv1" tvg-logo="" group-title="cctv",cctv1综合
-http://118.248.167.203:8088/tsfile/live/0001_1.m3u8
-#EXTINF:-1 tvg-id="cctv2" tvg-logo="" group-title="cctv",cctv2财经
-http://171.213.203.181:8088/udp/239.93.1.23:6000
-#EXTINF:-1 tvg-id="cctv7" tvg-logo="" group-title="cctv",cctv7国防军事
-http://1.58.83.167:8886/udp/229.58.190.132:5000
-#EXTINF:-1 tvg-id="cctv13" tvg-logo="" group-title="cctv",cctv13新闻
-http://223.166.234.215:7777/tsfile/live/0013_1.m3u8
